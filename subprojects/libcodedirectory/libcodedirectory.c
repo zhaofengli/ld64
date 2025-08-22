6e0db0c342007e60eab6303c3202519bb4ed2dd6
Zhaofeng Li <hello@zhaofeng.li>
2025-08-22 01:29:05 -0400

    [debug] Disable parallelism in libcodedirectory

--- subprojects/libcodedirectory/libcodedirectory.c
+++ subprojects/libcodedirectory/libcodedirectory.c
@@ -19,7 +19,7 @@
 
 #define _libcd_err(msg, ...) _libcd_err_log("%s: " msg "\n", __func__, ##__VA_ARGS__)
 
-#define LIBCD_PARALLEL 1
+#define LIBCD_PARALLEL 0
 
 #if LIBCD_PARALLEL
 #include <dispatch/dispatch.h>
